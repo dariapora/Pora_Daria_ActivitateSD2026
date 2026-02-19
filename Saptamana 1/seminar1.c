9d1e52f8e1f8186c1226e629bfe18e8aa3161fc3
dariapora <185256277+dariapora@users.noreply.github.com>
2026-02-19 13:57:00 +0200

    completare seminar 1

--- Saptamana 1/seminar1.c
+++ Saptamana 1/seminar1.c
@@ -1,35 +1,60 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-struct Sablon {
+struct Produs {
 	int id;
-	//int ;
-	//char* ;
-	//float ;
-	//char ;
+	int stoc;
+	char* denumire;
+	float pret;
+	char categorie;
 };
 
-struct Sablon initializare(/*parametri necesari*/) {
-	struct Sablon s;
-	s.id = 1;
-	//initializare structura
-	return s;
+struct Produs initializare(int id, int stoc, char* denumire, float pret, char categorie) {
+	struct Produs p;
+	p.id = id;
+	p.stoc = stoc;
+	p.pret = pret;
+	p.denumire = (char*)malloc(sizeof(char)*(strlen(denumire)+1));
+	strcpy(p.denumire, denumire);
+	p.categorie = categorie;
+	return p;
 }
 
-void afisare(struct Sablon s) {
-	//afisarea tuturor atributelor.
+void afisare(struct Produs p) {
+	printf("ID: %d\n", p.id);
+	printf("Stoc: %d\n", p.stoc);
+	printf("Pret: %5.2f\n", p.pret);
+	printf("Denumire: %s\n", p.denumire);
+	printf("Categorie: %c\n", p.categorie);
 }
 
-void modifica_Atribut(struct Sablon s) {
-	//modificarea unui atribut
+void modificaDenumire(struct Produs *p, char* nouaDenumire) {
+	free(p->denumire);
+	p->denumire = (char*)malloc(sizeof(char)*(strlen(nouaDenumire)+1));
+	strcpy(p->denumire, nouaDenumire);
 }
 
-void dezalocare(struct sablon *s) {
-	//dezalocare campuri alocate dinamic
+void dezalocare(struct Produs *p) {
+	if (p->denumire != NULL) {
+		free(p->denumire);
+		p->denumire=NULL;
+	}
+}
+
+float calculeazaValoare(struct Produs p) {
+	return p.stoc * p.pret;
 }
 
 int main() {
-	struct Sablon s;
-
+	struct Produs p;
+	p = initializare(1, 20, "Telefon", 2000, 'A');
+	afisare(p);
+	modificaDenumire(&p, "Mouse");
+	afisare(p);
+	printf("Valoarea totala pentru produsul %s este: %5.2f.\n", p.denumire, calculeazaValoare(p));
+	struct Produs* ps = &p; // alocat pe stiva main
+	struct Produs* pp = malloc(sizeof(struct Produs)); // alocat pe heap, trebuie dezalocat
+	dezalocare(&p);
 	return 0;
 }
